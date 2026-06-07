80975d710c5b4939dd4d14c01fa12d6b5ab7a731
kamalesh <kamalesh@skyraan.com>
2026-06-07 21:51:21 +0530

    changes in swager output

--- src/config/db.js
+++ src/config/db.js
@@ -4,9 +4,9 @@ import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "political",
+  user: process.env.DB_USER || "political",
+  password: process.env.DB_PASSWORD || "BHs5$--qOTZ=E4D-",
+  database: process.env.DB_NAME || "political_db",
   waitForConnections: process.env.DB_WAIT_FOR_CONNECTIONS,
   connectionLimit: Number(process.env.DB_CONNECTION_LIMIT) || 10,
   queueLimit: Number(process.env.DB_QUEUE_LIMIT) || 0,
