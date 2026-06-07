5769a67f3c0bf8f4f6058e898976caa3f71bc533
kamalesh <kamalesh@skyraan.com>
2026-06-07 22:04:35 +0530

    changes in db

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,10 @@ dotenv.config();
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host:"localhost",
-  user:"political",
-  password:"BHs5$--qOTZ=E4D-",
-  database:"political_db",
+  host: "localhost",
+  user: "political",
+  password: "BHs5$--qOTZ=E4D-",
+  database: "skyraantech_political_db",
   waitForConnections: process.env.DB_WAIT_FOR_CONNECTIONS,
   connectionLimit: Number(process.env.DB_CONNECTION_LIMIT) || 10,
   queueLimit: Number(process.env.DB_QUEUE_LIMIT) || 0,
@@ -22,5 +22,7 @@ const pool = mysql.createPool({
 //   queueLimit: Number(process.env.DB_QUEUE_LIMIT) || 0,
 //   timezone: "Z",
 // });
+
+
 
 export default pool;
