7d6e60a1cefd9d16501278869782e632ea7da235
kamalesh <kamalesh@skyraan.com>
2026-06-08 12:47:40 +0530

    changes in email.js

--- src/config/email.js
+++ src/config/email.js
@@ -16,6 +16,8 @@ dotenv.config();
 //     pass: process.env.MAIL_PASS,
 //   },
 // };
+
+
 const configOptions = {
   host: `mail.skyraantech.com`,
   port: 465,
@@ -25,13 +27,15 @@ const configOptions = {
     pass: `mRmSm*^5a9AIleR6`,
   },
 };
+
+
 export const transporter = nodemailer.createTransport(configOptions);
 
 export const sendMail = async (email, otp) => {
   try {
     const mailContent = await emailOtpTemplate(otp);
     const mailOptions = {
-      from: process.env.MAIL_USER,
+      from:"political@skyraantech.com",
       to: `${email}`,
       subject: "OTP Verification",
       html: mailContent,
@@ -48,7 +52,7 @@ export const sendContactUsMail = async (email, comments) => {
   try {
     const mailContent = await emailContactUsTemplate(comments);
     const mailOptions = {
-      from: process.env.MAIL_USER,
+      from:"political@skyraantech.com",
       to: `${email}`,
       subject: "OTP Verification",
       html: mailContent,
