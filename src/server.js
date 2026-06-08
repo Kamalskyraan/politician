29ef94cf0013861413d72e4c46b500c4cad2fa1f
kamalesh <kamalesh@skyraan.com>
2026-06-08 10:43:33 +0530

    changes in server.js

--- src/server.js
+++ src/server.js
@@ -16,16 +16,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api", router);
 
 app.use("/uploads", express.static("src/uploads"));
 
-app.get("/", (req, res) => {
+app.get("/check", (req, res) => {
   res.send("backend server is running now");
 });
-
 const port = process.env.PORT || 3000;
 
 app.listen(port, () =>
