89b0b19dfa244dd54492de796a125d5283fe5de2
kamalesh <kamalesh@skyraan.com>
2026-06-30 18:22:10 +0530

    changes in notification

--- src/service/notification.service.js
+++ src/service/notification.service.js
@@ -4,7 +4,7 @@ import fs from "fs";
 import { fileURLToPath } from "url";
 import { apnProvider } from "./apnProvider.js";
 import { executeQuery } from "../utils/helper.js";
-
+import apn from "apn";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -23,42 +23,61 @@ if (!admin.apps.length) {
 
 export const sendPushNotification = async ({ user_id, payload }) => {
   try {
+    console.log("Searching devices for user:", user_id);
+
     const devices = await executeQuery(
       `SELECT device_token, device_type
        FROM user_device
-       WHERE user_id = ? AND device_token IS NOT NULL`,
+       WHERE user_id = ?
+       AND device_token IS NOT NULL`,
       [user_id],
     );
 
-    if (!devices.length) return;
+    console.log("Devices:", devices);
+
+    if (!devices.length) {
+      console.log("No registered devices");
+      return;
+    }
 
     const androidTokens = [];
     const iosTokens = [];
 
     for (const d of devices) {
-      if (d.device_type === "android") {
+      console.log(d.device_type, d.device_token);
+
+      if (d.device_type?.toLowerCase() === "android") {
         androidTokens.push(d.device_token);
-      } else if (d.device_type === "ios") {
+      }
+
+      if (d.device_type?.toLowerCase() === "ios") {
         iosTokens.push(d.device_token);
       }
     }
 
+    console.log("Android:", androidTokens);
+    console.log("iOS:", iosTokens);
+
     if (androidTokens.length) {
-      await admin.messaging().sendEachForMulticast({
+      const result = await admin.messaging().sendEachForMulticast({
         tokens: androidTokens,
         notification: {
           title: payload.title,
           body: payload.message,
         },
       });
+
+      console.log("Android Result:", result);
     }
 
     if (iosTokens.length) {
-      await sendAPNSNotification({
+      const result = await sendAPNSNotification({
         tokens: iosTokens,
         title: payload.title,
         body: payload.message,
       });
+
+      console.log("APNS Result:", result);
     }
   } catch (err) {
     console.error("Push Notification Error:", err);
@@ -80,8 +99,7 @@ export const sendFCMNotification = async ({ token, title, body, data }) => {
 
 export const sendAPNSNotification = async ({ tokens, title, body }) => {
   try {
-
-    console.log("from ios notification")
+    console.log("from ios notification");
     const notification = new apn.Notification();
 
     notification.alert = { title, body };
