d39d22351d19842e7f796ac18ff730b71e164de7
kamalesh <kamalesh@skyraan.com>
2026-07-03 17:52:36 +0530

    changes in filename

--- src/controller/meeting.controller.js
+++ src/controller/meeting.controller.js
@@ -153,10 +153,13 @@ export const getMembers = async (req, res) => {
     // }
 
     await addNotification("MEETING_CREATED", user_id, "meeting", 1);
-    await sendPushNotification({user_id, payload:{
-      title: "Meeting created",
-      message: "New meeting has been created",
-    }});
+    await sendPushNotification({
+      user_id,
+      payload: {
+        title: "Members Fetched",
+        message: "Members Fetched successfully",
+      },
+    });
     console.log("first");
     return sendResponse(res, 200, 1, "Member fetched successfully", data, "");
   } catch (error) {
