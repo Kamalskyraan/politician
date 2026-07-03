2f46ee42b53fbe6923a60bce91417f460b3c7d39
kamalesh <kamalesh@skyraan.com>
2026-07-03 12:24:24 +0530

    changes in swagger

--- src/config/db.js
+++ src/config/db.js
@@ -2,17 +2,17 @@ import dotenv from "dotenv";
 dotenv.config();
 import mysql from "mysql2/promise";
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "skyraantech_political",
-  password: "BHs5$--qOTZ=E4D-",
-  database: "skyraantech_political_db",
-  waitForConnections: process.env.DB_WAIT_FOR_CONNECTIONS,
-  connectionLimit: Number(process.env.DB_CONNECTION_LIMIT) || 10,
-  queueLimit: Number(process.env.DB_QUEUE_LIMIT) || 0,
-  // timezone: "Z",
-  dateStrings: true,
-});
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "skyraantech_political",
+//   password: "BHs5$--qOTZ=E4D-",
+//   database: "skyraantech_political_db",
+//   waitForConnections: process.env.DB_WAIT_FOR_CONNECTIONS,
+//   connectionLimit: Number(process.env.DB_CONNECTION_LIMIT) || 10,
+//   queueLimit: Number(process.env.DB_QUEUE_LIMIT) || 0,
+//   // timezone: "Z",
+//   dateStrings: true,
+// });
 
 // const pool = mysql.createPool({
 //   host: "localhost",
@@ -26,15 +26,15 @@ const pool = mysql.createPool({
 // });
 //
 
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "skyraancloud_political_db",
-//   password: "c]dx5#=DL)%36.b}",
-//   database: "skyraancloud_kamalesh_sky",
-//   waitForConnections: process.env.DB_WAIT_FOR_CONNECTIONS,
-//   connectionLimit: Number(process.env.DB_CONNECTION_LIMIT) || 10,
-//   queueLimit: Number(process.env.DB_QUEUE_LIMIT) || 0,
-//   // timezone: "Z",
-//   dateStrings: true,
-// });
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "skyraancloud_political_db",
+  password: "c]dx5#=DL)%36.b}",
+  database: "skyraancloud_kamalesh_sky",
+  waitForConnections: process.env.DB_WAIT_FOR_CONNECTIONS,
+  connectionLimit: Number(process.env.DB_CONNECTION_LIMIT) || 10,
+  queueLimit: Number(process.env.DB_QUEUE_LIMIT) || 0,
+  // timezone: "Z",
+  dateStrings: true,
+});
 export default pool;
