935ed2b364591a5854703274db6e538194c12c5e
kamalesh <kamalesh@skyraan.com>
2026-07-08 15:12:27 +0530

    changes in task

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -101,7 +101,7 @@ export class taskModel {
 
       query = `SELECT id, title, descp, t_priority, from_date, to_date, media_id, attnds_id, t_status, is_remind, remind_status, remind_tenure, remind_at, snooze_at, nxt_snooze_at FROM tasks WHERE user_id = ? AND t_status IN (${placeholders}) ORDER BY from_date LIMIT ? OFFSET ?`;
       params.push(user_id, ...status, limit, offset);
-      
+      console.log("QUERY , PRINTED", query);
     } else {
       countQuery = `SELECT FROM tasks WHERE user_id = ? LIMIT ? OFFSET ?`;
       countParams.push(user_id);
@@ -114,7 +114,8 @@ export class taskModel {
     const total = countResult?.data[0]?.total;
 
     const result = await executeQuery(query, params);
-        if (result?.success === 1) {
+    console.log(result, "result");
+    if (result?.success === 1) {
       return {
         success: 1,
         data: result?.data,
