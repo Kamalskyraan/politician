838a4f468a278c793906937934742aec4054b85f
kamalesh <kamalesh@skyraan.com>
2026-07-16 10:53:46 +0530

    changes in task controller

--- src/service/apnProvider.js
+++ src/service/apnProvider.js
@@ -13,9 +13,9 @@ const __dirname = path.dirname(__filename);
 export const apnProvider = new apn.Provider({
   token: {
     key: path.join(__dirname, "AuthKey_G2HUL3X6FP.p8"),
-    keyId: process.env.IOS_KEY_ID,
-    teamId: process.env.IOS_TEAM_ID,
+    keyId:"G2HUL3X6FP",
+    teamId:"38BKAW23ZM",
   },
   // production: process.env.NODE_ENV === "production" ? true : false,
-  production: false,
+  production: true,
 });
